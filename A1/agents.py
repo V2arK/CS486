4028a12e59878171ec271ef72bd9504b1d4f4468
v2ark <Caohonglin317@hotmail.com>
2024-01-25 01:00:01 -0500

    change my_heuristic

--- A1/agents.py
+++ A1/agents.py
@@ -208,7 +208,9 @@ def my_heuristic(state: State, max_role: str):
         
     #If the state is not terminal, give the heuristic evaluation
     # Define the scoring for different lengths of contiguous lines
-    scores = {1: 0.05, 2: 0.35, 3: 0.99}
+    scores = {0:0, 1: 0.05, 2: 0.35, 3: 0.99}
+    empty_scores = {0:0, 1: 0.49, 2: 0.17, 3: 0.03}
+    
     directions = [(0, 1), (1, 0), (1, 1), (1, -1)]  
     # no need to add the rest directions, as they are just the opposite direction for which we checked.
 
@@ -220,14 +222,22 @@ def my_heuristic(state: State, max_role: str):
                     for dx, dy in directions:
                         # Determine if we need to check in this direction
                         if col + (dx * 3) < state.num_cols and row + (dy * 3) < state.num_rows and row + (dy * 3) >= 0:
-                            for length in range(3, 0, -1):  # Check for 3-in-a-row, 2-in-a-row, 1-in-a-row
-                                if all(state.board[col + dx * i][row + dy * i] == player for i in range(length)):
-                                       # above line check if the the rest checker in this direction is ours
-                                    count += scores[length]
-                                    break  
-                                    # Early termination if we found the longest line, so we will not eg: 
-                                    # add scores for 1-in-a-row, 2-in-a-row, 3-in-a-row in the same direction.
+                            for length in range(4, 0, -1):  # Check for 3-in-a-row, 2-in-a-row, 1-in-a-row
+                                for i in range(length):
+                                    checker_count = 0
+                                    empty_count = 0
                                     
+                                    if state.board[col + dx * i][row + dy * i] == player:
+                                        checker_count += 1
+                                    elif state.board[col + dx * i][row + dy * i] == player:
+                                        empty_count += 1
+                                    else:
+                                        continue
+                                    
+                                    count += scores[checker_count]
+                                    count += empty_scores[empty_count]
+                                    break
+            
         return count
 
     max_material = count_lines(max_role)
@@ -356,7 +366,7 @@ if __name__ == "__main__":
     # This is the code that gets run when you run this file. You can set up games to be played here.
 
     #game = Game(HumanPlayer(), MinimaxPlayer(4, three_line_heur))
-    game = Game(RandomPlayer(),MinimaxPlayer(4, my_heuristic))
+    game = Game( MinimaxPlayer(4, three_line_heur),MinimaxPlayer(4, my_heuristic))
     # Here are some more examples of game initialization:
     # game = Game(MinimaxPlayer(4, three_line_heur), MinimaxPlayer(4, zero_heur))
     # game = Game(RandomPlayer(), FirstMovePlayer())
